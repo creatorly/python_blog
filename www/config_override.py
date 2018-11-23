37ad1258f52d6306c191cc05cd579586bbd7a783
linye <creator_ly@163.com>
2018-11-23 17:45:56 +0800

    add webframe config and mvc

---
+++ www/config_override.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+'''
+Override configuration.
+修改项
+'''
+
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+        'user': 'root',
+		'password': 'root'
+	}
+}
