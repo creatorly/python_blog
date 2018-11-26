ad98ec31bb22c20168e0023ebc69e14c5c2870c7
linye <creator_ly@163.com>
2018-11-26 14:22:54 +0800

    add register and signin func

--- www/config.py
+++ www/config.py
@@ -1,56 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 '''
 Configuration
 '''
 
+__author__ = 'Michael Liao'
 
 import config_default
 
-
 class Dict(dict):
-	' Simple dict but support access as x.y style. '
-	def __init__(self, names=(), values=(), **kw):
-		super().__init__(**kw)
-		for k, v in zip(names, values):
-			self[k] = v
-	def __getattr__(self, key):
-		try:
-			return self[key]
-		except KeyError:
-			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
-	def __setattr__(self, key, value):
-		self[key] = value
+    '''
+    Simple dict but support access as x.y style.
+    '''
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
 
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
 
 def merge(defaults, override):
-	' 读取更新的配置 '
-	r = {}
-	for k, v in defaults.items():
-		if k in override:
-			if isinstance(v, dict):
-				r[k] = merge(v, override[k])
-			else:
-				r[k] = override[k]
-		else:
-			r[k] = v
-	return r
-
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
 
 def toDict(d):
-	' 转换成Dict类 '
-	D = Dict()
-	for k, v in d.items():
-		D[k] = toDict(v) if isinstance(v, dict) else v
-	return D
-
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
 
 configs = config_default.configs
+
 try:
-	import config_override
-	cofigs = merge(configs, config_override.configs)
+    import config_override
+    configs = merge(configs, config_override.configs)
 except ImportError:
-	pass
-configs = toDict(configs) # configs.attr
+    pass
+
+configs = toDict(configs)
